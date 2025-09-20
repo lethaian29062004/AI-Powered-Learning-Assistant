28d308889b2f24e4c9928fea4f34dc89367480dc
Thai An Le <10422003@student.vgu.edu.vn>
2025-09-20 12:04:41 +0700

    update app.py

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object("config.Config")
 
-    # Import và register blueprint
+    # Import and register blueprint
     from routes.main_routes import main_bp
     app.register_blueprint(main_bp)
 
     return app
 
+app = create_app() 
+
 if __name__ == "__main__":
-    app = create_app()
     app.run(host="0.0.0.0", port=5000, debug=True)
